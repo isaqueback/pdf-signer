741a30a750f19500798c6cee8b3d4e33073664fa
Isaque Delfino <isa.quecosta00@gmail.com>
2023-12-21 13:59:15 +0000

    pdf signer completed

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log('Hello World!')
+import { readFileSync, writeFileSync } from 'node:fs'
+import * as path from 'node:path'
+import signpdf from '@signpdf/signpdf'
+import { P12Signer } from '@signpdf/signer-p12'
+
+function work() {
+  const inputPdfPath = path.join(
+    __dirname,
+    'test-assets',
+    'with-placeholder.pdf',
+  )
+  const certificatePath = path.join(__dirname, 'test-assets', 'certificate.p12')
+
+  // Pdf para ser assinado
+  const inputPdf = readFileSync(inputPdfPath)
+  // Certificado que vai ser usado para assinar o pdf
+  const certificate = readFileSync(certificatePath)
+
+  const signer = new P12Signer(certificate)
+
+  // Assinando pdf e criando pdf assinado
+  signpdf
+    // Assinar o pdf com o certificado
+    .sign(inputPdf, signer)
+    // signedPdf é o pdf assinado digitalmente em forma de Buffer
+    .then(function (signedPdf: Buffer) {
+      const outputPdfPath = path.join(
+        __dirname,
+        'test-assets',
+        'signed-pdf.pdf',
+      )
+      // Criar o pdf assinado digitalmente
+      writeFileSync(outputPdfPath, signedPdf)
+    })
+}
+
+work()
